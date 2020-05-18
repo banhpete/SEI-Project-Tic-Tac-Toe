63b6f924f5f84b7c35ab4e1c7b303b18008650db
Pebanh <peter.banh95@gmail.com>
2020-05-17 21:11:22 -0400

    V0 - Initial Release of Tic Tac Toe

--- main.js
+++ main.js
@@ -1,177 +1,170 @@
-// Initial Prompt to Grab Names
-var player1Name = prompt("Player 1, Enter your Name");
-var player2Name = prompt("Player 2, Enter your Name");
-
 // Initialize Game Object with properties and methods
+console.log("reinit");
 var gameObj = {
   // State
   playerGrid: [],
-  currentTurn: player1Name,
-  DOMGameBoard: document.getElementById("game-board"),
-  DomTurnHeading: document.getElementById("player-turn"),
-  numOfSquares: parseInt(prompt("Enter number of squares")),
-  player1Name,
-  player2Name,
-  turnValues: {
-    [this.player1Name]: 1,
-    [this.player2Name]: -1,
+  gameStatus: "Ongoing",
+  currentPlayer: "player1",
+  DOMGameBoard: undefined,
+  DOMTurnHeading: undefined,
+  numOfSquares: 0,
+  numOfTurns: 0,
+  player1: {
+    name: undefined,
+    turnValue: 1,
+    playerIcon: "circle",
   },
-  playerIcon: {
-    [this.player1Name]: "circle",
-    [this.player2Name]: "cross",
+  player2: {
+    name: undefined,
+    turnValue: -1,
+    playerIcon: "cross",
   },
   //Methods
-  initialize: function () {
-    this.DomTurnHeading.innerText = gameObj.currentTurn + "'s Turn";
-    this.insertSquares(this.numOfSquares);
-    this.DOMGameBoard.addEventListener("click", this.handleClicks);
+  initializeGame: function () {
+    document.querySelector("#game-menu button").onclick = gameObj.inputSettings;
+    gameObj.initializeState();
   },
-  insertSquares: function (numOfSquares) {
-    for (i = 0; i < numOfSquares; i++) {
+  inputSettings: function (event) {
+    console.log("hi");
+    event.preventDefault();
+    gameObj.player1.name = document.querySelector("#player1-name").value
+      ? document.querySelector("#player1-name").value
+      : "Player 1";
+    gameObj.player2.name = document.querySelector("#player2-name").value
+      ? document.querySelector("#player2-name").value
+      : "Player 2";
+    gameObj.numOfSquares = document.querySelector("#three").checked
+      ? 3
+      : document.querySelector("#four").checked
+      ? 4
+      : document.querySelector("#five").checked
+      ? 5
+      : 3;
+    document.querySelector("#game-menu").style.visibility = "hidden";
+    document.documentElement.style.setProperty(
+      "--background-color",
+      "rgba(0,0,0,0.3)"
+    );
+    gameObj.initializeState();
+  },
+  initializeState: function () {
+    // Setting up state
+    gameObj.DOMGameBoard = document.getElementById("game-board");
+    gameObj.DOMTurnHeading = document.getElementById("player-turn");
+    gameObj.numOfTurns = gameObj.numOfSquares - 2;
+    gameObj.playerGrid = gameObj.createGrid(gameObj.numOfSquares);
+
+    // Rendering Heading
+    gameObj.renderTurnHeading(gameObj.currentPlayer);
+
+    // Adding Event Listeners
+    gameObj.DOMGameBoard.addEventListener("click", gameObj.handleClicks);
+    document.getElementById("reset").onclick = gameObj.resets;
+  },
+  createGrid: function (gridLength) {
+    let grid = [];
+    for (i = 0; i < gridLength; i++) {
       let row = [];
-      for (j = 0; j < numOfSquares; j++) {
+      for (j = 0; j < gridLength; j++) {
         row.push(0);
-        let square = document.createElement("div");
-        square.style.height = `calc(100%/${numOfSquares})`;
-        square.style.width = `calc(100%/${numOfSquares})`;
-        square.style.border = "2px solid purple";
-        square.classList.add("unselected-square");
-        square.id = i + "," + j;
-        this.DOMGameBoard.appendChild(square);
+        this.renderSquare(i, j);
       }
-      this.playerGrid.push(row);
+      grid.push(row);
     }
+    return grid;
+  },
+  renderTurnHeading: (player) =>
+    (gameObj.DOMTurnHeading.innerText =
+      gameObj[player].name + "'s Turn - " + gameObj.numOfTurns + " move left"),
+  renderTieHeading: () =>
+    (gameObj.DOMTurnHeading.innerText = "It's a Tie! Restart the game!"),
+  renderWinHeading: (player) =>
+    (gameObj.DOMTurnHeading.innerText =
+      gameObj[player].name + " Won the Game!"),
+  renderSquare: function (i, j) {
+    let square = document.createElement("div");
+    square.style.height = `calc(100%/${gameObj.numOfSquares})`;
+    square.style.width = `calc(100%/${gameObj.numOfSquares})`;
+    square.style.border = "4px solid 	#08679f";
+    square.classList.add("unselected-square");
+    square.id = i + "," + j;
+    this.DOMGameBoard.appendChild(square);
+  },
+  reRenderSquare: function (square) {
+    square.classList.remove("unselected-square");
+    square.classList.add(gameObj[gameObj.currentPlayer].playerIcon);
+  },
+  resets: function () {
+    gameObj.numOfTurns = gameObj.numOfSquares - 2;
+    gameObj.renderTurnHeading(gameObj.currentPlayer);
+    gameObj.gameStatus = "ongoing";
+    gameObj.playerGrid = [];
+    for (i = 0; i < gameObj.numOfSquares; i++) {
+      let row = [];
+      for (j = 0; j < gameObj.numOfSquares; j++) {
+        row.push(0);
+        let square = document.getElementById(i + "," + j);
+        square.classList.remove("cross");
+        square.classList.remove("circle");
+        square.classList.add("unselected-square");
+      }
+      gameObj.playerGrid.push(row);
+    }
+  },
+  updateTurn: function () {
+    gameObj.numOfTurns = gameObj.numOfSquares - 2;
+    gameObj.currentPlayer =
+      gameObj.currentPlayer === "player1" ? "player2" : "player1";
+    gameObj.renderTurnHeading(gameObj.currentPlayer);
+  },
+  updateGrid: function (row, column) {
+    gameObj.playerGrid[row][column] = gameObj[gameObj.currentPlayer].turnValue;
+  },
+  updateGameStatus: function () {
+    let tieCheck = 1;
+    let diagSum1 = 0;
+    let diagSum2 = 0;
+    for (i = 0; i < gameObj.numOfSquares; i++) {
+      let rowSum = 0;
+      let columnSum = 0;
+      diagSum1 += gameObj.playerGrid[i][i];
+      diagSum2 += gameObj.playerGrid[i][gameObj.numOfSquares - i - 1];
+      for (j = 0; j < gameObj.numOfSquares; j++) {
+        rowSum += gameObj.playerGrid[i][j];
+        columnSum += gameObj.playerGrid[j][i];
+        tieCheck *= gameObj.playerGrid[j][i];
+      }
+      if (
+        Math.abs(rowSum) == gameObj.numOfSquares ||
+        Math.abs(columnSum) == gameObj.numOfSquares ||
+        Math.abs(diagSum1) == gameObj.numOfSquares ||
+        Math.abs(diagSum2) == gameObj.numOfSquares
+      )
+        return "win";
+    }
+    if (tieCheck) return "tie";
+    return "ongoing";
   },
   handleClicks: function (event) {
     event.stopPropagation();
     let square = event.target;
-    if (!square.classList.value.includes("unselected-square")) {
-      console.log("Pick Another");
+    if (
+      !square.classList.value.includes("unselected-square") ||
+      gameObj.gameStatus == "win"
+    )
       return;
-    }
-    square.classList.remove("unselected-square");
-    square.classList.add(gameObj.playerIcon[gameObj.currentTurn]);
+    gameObj.reRenderSquare(square);
     let coord = square.id.split(",");
     gameObj.updateGrid(coord[0], coord[1]);
-    gameObj.checkWinner();
-    gameObj.updateTurn();
-  },
-  updateTurn: function () {
-    gameObj.currentTurn =
-      gameObj.currentTurn === gameObj.player1Name
-        ? gameObj.player2Name
-        : gameObj.player1Name;
-    this.DomTurnHeading.innerText = gameObj.currentTurn + "'s Turn";
-  },
-  updateGrid: function (row, column) {
-    gameObj.playerGrid[row][column] = gameObj.turnValues[gameObj.currentTurn];
-  },
-  checkWinner: function () {
-    for (i = 0; i < 3; i++) {
-      let rowSum = 0;
-      let columnSum = 0;
-      console.log(gameObj.playerGrid);
-      for (j = 0; j < 3; j++) {
-        rowSum += gameObj.playerGrid[i][j];
-        columnSum += gameObj.playerGrid[j][i];
-      }
-      console.log(rowSum, columnSum);
-      if (rowSum == 3 || columnSum == 3) {
-        console.log("Player 1 Wins");
-      } else if (rowSum == -3 || columnSum == -3) {
-        console.log("Player 2 Wins");
-      }
-    }
+    gameObj.numOfTurns--;
+    gameObj.renderTurnHeading(gameObj.currentPlayer);
+    gameObj.gameStatus = gameObj.updateGameStatus();
+    if (gameObj.gameStatus === "win")
+      gameObj.renderWinHeading(gameObj.currentPlayer);
+    else if (gameObj.gameStatus === "tie") gameObj.renderTieHeading();
+    else if (gameObj.numOfTurns === 0) gameObj.updateTurn();
   },
 };
 
-gameObj.initialize();
-
-// insertSquares();
-// console.log(playerGrid);
-// gameBoard.onclick = handleClicks;
-
-// // Function
-// // Create Grid - To add a twist, allow player to enter what grid size they want, 3x3, 4x4 or 5x5
-// // Create nested for loop to add squares to the gameboard
-// // Each will have their own index as an id representing an element in the Game Grid. 0,0 will be the top left
-// // PlayerGrid will be created during this function since the array is dependent on the layout of the grid
-// function insertSquares(numOfSquares = 5) {
-//   console.log("Squares Loaded");
-//   for (i = 0; i < numOfSquares; i++) {
-//     let row = [];
-//     for (j = 0; j < numOfSquares; j++) {
-//       row.push(0);
-//       let square = document.createElement("div");
-//       square.style.height = `calc(100%/${numOfSquares})`;
-//       square.style.width = `calc(100%/${numOfSquares})`;
-//       square.style.border = "2px solid purple";
-//       square.classList.add("unselected-square");
-//       square.id = i + "," + j;
-//       gameBoard.appendChild(square);
-//     }
-//     playerGrid.push(row);
-//   }
-// }
-
-// // Function
-// // Event Handler for Clicks
-// // Apply on gameboard and listen for events to bubble up
-// // Once a div is clicked, it's class will change depending on who's turn.
-// // ID of the div is extracted to update the playerGrid
-// // Call function to check if player has won
-// // If no one won, change turn
-// function handleClicks(event) {
-//   event.stopPropagation();
-//   let square = event.target;
-//   if (!square.classList.value.includes("unselected-square")) {
-//     console.log("Pick Another");
-//     return;
-//   }
-//   console.log(typeof square.classList.value);
-//   square.classList.remove("unselected-square");
-//   square.classList.add(playerIcon[this.currentTurn]);
-//   let coord = square.id.split(",");
-//   updateGrid(coord[0], coord[1]);
-//   checkWinner();
-//   this.currentTurn = updateTurn(this.currentTurn);
-// }
-
-// function updateGrid(row, column) {
-//   playerGrid[row][column] = turnValues[this.currentTurn];
-//   console.log(playerGrid);
-// }
-
-// // Function
-// // Update playerturn
-// function updateTurn(currentPlayer) {
-//   let newPlayer = currentPlayer == "Player 1" ? "Player 2" : "Player 1";
-//   turnHeading = document.getElementById("player-turn");
-//   turnHeading.innerText = newPlayer + " Turn";
-//   return newPlayer;
-// }
-
-// // Funcution
-// // Check if there is a winner
-// // Go through grid and perform math operator to see if any row or column has a product of 1
-// // Check Diagonal Conditions as well
-// function checkWinner() {
-//   for (i = 0; i < 3; i++) {
-//     let rowSum = 0;
-//     let columnSum = 0;
-//     for (j = 0; j < 3; j++) {
-//       rowSum += playerGrid[i][j];
-//       columnSum += playerGrid[j][i];
-//     }
-//     console.log(rowSum, columnSum);
-//     if (rowSum == 3 || columnSum == 3) {
-//       console.log("Player 1 Wins");
-//     } else if (rowSum == -3 || columnSum == -3) {
-//       console.log("Player 2 Wins");
-//     }
-//   }
-// }
-
-// insertSquares();
-// console.log(playerGrid);
-// gameBoard.onclick = handleClicks;
+// Initialize Game
+gameObj.initializeGame();
